914bf4a1242ef7c5a64af601ca14421b140ae1bb
Livlivm <liviaampereira@gmail.com>
2026-04-22 08:58:38 -0300

    Add files via upload

---
+++ lib/pallet.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const background = Color.fromARGB(255, 225, 250, 255);
+  static const card = Color.fromARGB(255, 255, 255, 255);
+  static const primary = Color.fromARGB(255, 194, 221, 245);
+  static const shadow = Colors.black12;
+}
